cc49503f23ff57954789f56be461827de7071c6f
mali1998 <mali@arconme.com>
2024-09-22 19:17:03 +0300

    implement create method

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -1,7 +1,8 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
+import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer } from '@nestjs/websockets';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
+import { Socket, Server } from 'socket.io'
 
 @WebSocketGateway({
   cors: {
@@ -9,11 +10,18 @@ import { UpdateMessageDto } from './dto/update-message.dto';
   },
 })
 export class MessagesGateway {
+  @WebSocketServer()
+  server: Server;
+
   constructor(private readonly messagesService: MessagesService) { }
 
   @SubscribeMessage('createMessage')
-  create(@MessageBody() createMessageDto: CreateMessageDto) {
-    return this.messagesService.create(createMessageDto);
+  async create(@MessageBody() createMessageDto: CreateMessageDto) {
+    const message = await this.messagesService.create(createMessageDto)
+
+    this.server.emit('message', message);
+
+    return message;
   }
 
   @SubscribeMessage('findAllMessages')
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
+import { Message } from './entities/message.entity';
 
 @Injectable()
 export class MessagesService {
+  messages: Message[] = [{ name: 'Hamada', text: 'Hello' }]
   create(createMessageDto: CreateMessageDto) {
-    return 'This action adds a new message';
+    const message = { ...createMessageDto };
+    this.messages.push(message);
+
+    return message;
   }
 
   findAll() {
