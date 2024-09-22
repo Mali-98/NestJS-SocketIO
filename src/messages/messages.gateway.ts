0b23cca00305465e889a8a5173a3350dbf3b5b66
mali1998 <mali@arconme.com>
2024-09-22 19:08:24 +0300

    enable websocket

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -3,9 +3,13 @@ import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
 export class MessagesGateway {
-  constructor(private readonly messagesService: MessagesService) {}
+  constructor(private readonly messagesService: MessagesService) { }
 
   @SubscribeMessage('createMessage')
   create(@MessageBody() createMessageDto: CreateMessageDto) {
